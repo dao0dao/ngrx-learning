aec567d6e8be50aa2197d7fbbc116390bc0434af
Demid Greshnikov <demid.westhill@gmail.com>
2024-06-01 11:38:01 +0200

    init

--- src/app/import.providers.from.ts
+++ src/app/import.providers.from.ts
@@ -1,10 +1,8 @@
 import { importProvidersFrom } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 export const provideImports = importProvidersFrom([
   ButtonsModule.forRoot(),
-  StoreModule.forRoot(),
   BrowserAnimationsModule,
 ]);
